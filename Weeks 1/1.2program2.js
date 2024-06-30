2d5e47e428c011c766cd8a6cc73e1629d44917ec
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-06-30 13:46:58 +0530

    Add files via upload

---
+++ Weeks 1/1.2program2.js
@@ -0,0 +1,6 @@
+//Write a program prints the biggest number.
+let arr = [3, 6, 2, 56, 32, 5, 89, 32];
+let largest = Math.max(...arr);
+
+console.log("The largest number is " + largest);
+//The Math.max() method returns the number with the highest value.
