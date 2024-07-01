c330029c57f2a978d6e6467a1ba0cdba5fffc1d6
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-07-01 14:37:50 +0530

    Add files via upload

---
+++ JS/Ternary.js
@@ -0,0 +1,6 @@
+//Ternary operators: A ternory operator is the only condition that takes three operands
+//syntax-->condition?true output:false output;
+let age = 25;
+// age>=18? console.log("adult") : console.log("not adult");-->we can also print like this
+let result = age>=18? "adult" : "not adult";
+console.log(result);
