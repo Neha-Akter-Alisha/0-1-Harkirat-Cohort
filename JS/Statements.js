70d4f9504cb6f3708d9621f65582dfd9b04b777f
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-07-01 14:15:53 +0530

    Add files via upload

---
+++ JS/Statements.js
@@ -0,0 +1,21 @@
+//Conditional statements
+let mode = "red";
+let color;
+if(mode==="dark"){
+    color = "black";
+}
+//If we want to check multiple condition we will use else if condition
+else if(mode==="pink"){
+    color = "pink";
+}
+else if(mode==="blue"){
+    color = "blue";
+}
+else{
+    color = "white";
+}
+console.log(color);
+if(mode==="red")
+{//Here we are using only single condition
+console.log(mode);
+}
